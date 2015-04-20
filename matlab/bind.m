2951ff87a60014e2ce2c22f244874cf16434d058
Nadav Geva <nadav1994@gmail.com>
2015-04-20 22:38:55 +0300

    Increase input buffer size to 4096

--- matlab/bind.m
+++ matlab/bind.m
@@ -3,4 +3,5 @@ s = serial('COM12','BaudRate',BAUD_RATE);
 s.BytesAvailableFcnMode = 'terminator';
 s.Terminator = 13; % New line character
 s.BytesAvailableFcn = @serial_callback;
+s.InputBufferSize = 4096;
 fopen(s);

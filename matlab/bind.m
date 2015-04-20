201aa57c6d7e501c42c57eb250dd35cc416d6dee
Nadav Geva <nadav1994@gmail.com>
2015-04-20 21:51:01 +0300

    Add terminator to be newline

--- matlab/bind.m
+++ matlab/bind.m
@@ -1,4 +1,5 @@
 BAUD_RATE = 115200;
 s = serial('COM12','BaudRate',BAUD_RATE);
 s.BytesAvailableFcnMode = 'terminator';
+s.Terminator = 13; % New line character
 s.BytesAvailableFcn = @serial_callback;

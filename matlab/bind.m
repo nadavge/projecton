5c775b934f073f0e4f03940dbad985eb3bded04e
Nadav Geva <nadav1994@gmail.com>
2015-04-20 21:49:07 +0300

    Add serial callback function

--- matlab/bind.m
+++ matlab/bind.m
@@ -1,4 +1,4 @@
 BAUD_RATE = 115200;
 s = serial('COM12','BaudRate',BAUD_RATE);
 s.BytesAvailableFcnMode = 'terminator';
-s.BytesAvailableFcn = @serialcallback;
+s.BytesAvailableFcn = @serial_callback;
---
+++ matlab/serial_callback.m
@@ -0,0 +1,7 @@
+function serial_callback()
+%SERIAL_CALLBACK Summary of this function goes here
+%   Detailed explanation goes here
+
+
+end
+

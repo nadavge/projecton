76e10a7ac618b41368f854d6b7130459b9dff884
Nadav Geva <nadav1994@gmail.com>
2015-04-20 22:05:59 +0300

    Fix project structure

--- matlab/serial_callback.m
+++ matlab/serial_callback.m
@@ -2,9 +2,5 @@ function serial_callback(obj, event)
 %SERIAL_CALLBACK Summary of this function goes here
 %   Detailed explanation goes here
     display(fscanf(obj));
-    display(event.Data);
-    %fopen(s);
-    %display fscanf(s)
-    %fclose(s);
 end
 

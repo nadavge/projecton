aa51f7e821067e7ff3e6524fe064acc8ff0314a9
Nadav Geva <nadav1994@gmail.com>
2015-04-28 23:28:22 +0300

    Fix indentation (change space to tabs)

--- TeensySimultaneousADC/TeensySimultaneousADC.ino
+++ TeensySimultaneousADC/TeensySimultaneousADC.ino
@@ -8,15 +8,15 @@
 void setup() 
 {
 
-  pinMode(LEDPIN, OUTPUT);
-  pinMode(A2, INPUT); 
-  highSpeed8bitADCSetup();
+	pinMode(LEDPIN, OUTPUT);
+	pinMode(A2, INPUT); 
+	highSpeed8bitADCSetup();
 
-  Serial.begin(115200);
-  //BLINK LED, WE ARE ALIVE
-  digitalWrite(LEDPIN,1);
-  delay(2000);
-  digitalWrite(LEDPIN,0);
+	Serial.begin(115200);
+	//BLINK LED, WE ARE ALIVE
+	digitalWrite(LEDPIN,1);
+	delay(2000);
+	digitalWrite(LEDPIN,0);
 }
 
 
@@ -44,90 +44,90 @@ long loopTime = 0;
 long loopStartTime = 0;
 bool tookTime = false;
 
-int i;
-int k;
+int i = 0;
+int k = 0;
 
 
 void loop() 
 {
-  startTime = micros();
-     //START SAMPLING
-     //Strange init in this for, but the compiler seems to optimize this code better, so we get faster sampling
-  loopStartTime = micros();
-  i = 0;
-  k = 0;
-  event = NO_EVENT;
-  for(;i<SAMPLES;i++) 
-  {
-    //TAKE THE READINGS
-    highSpeed8bitAnalogReadMacro(channelA2,channelA2,value1,value2);
-    
-    buffer[k] = value1;
-    
-    //CHECK FOR EVENTS
-    if (value1 > THRESHOLD && event != NO_EVENT) 
-    {
-      event = k;
-      // Set i to sample the amount set by the user
-      i = SAMPLES_EVENT;
-    }
-    
-    if (++k == BUFFERSIZE)
+	startTime = micros();
+	//START SAMPLING
+	//Strange init in this for, but the compiler seems to optimize this code better, so we get faster sampling
+	loopStartTime = micros();
+	i = 0;
+	k = 0;
+	event = NO_EVENT;
+	for(;i<SAMPLES;i++) 
 	{
-		k = 0;
-		loopTime = micros() - loopStartTime;
-                loopStartTime = micros();
+		//TAKE THE READINGS
+		highSpeed8bitAnalogReadMacro(channelA2,channelA2,value1,value2);
+		
+		buffer[k] = value1;
+		
+		//CHECK FOR EVENTS
+		if (value1 > THRESHOLD && event != NO_EVENT) 
+		{
+			event = k;
+			// Set i to sample the amount set by the user
+			i = SAMPLES_EVENT;
+		}
+		
+		if (++k == BUFFERSIZE)
+		{
+			k = 0;
+			loopTime = micros() - loopStartTime;
+			loopStartTime = micros();
+		}
 	}
-  }
-  stopTime = micros();
-  
-  //WAS AN EVENT BEEN DETECTED?
-  if (event != NO_EVENT) 
-  {
-    printInfo();
-    printSamples(); 
-  }
+	stopTime = micros();
+	
+	//WAS AN EVENT BEEN DETECTED?
+	if (event != NO_EVENT) 
+	{
+		printInfo();
+		printSamples(); 
+	}
 
-  //DID WE RECEIVE COMMANDS?
-  if (Serial.available()) parseSerial();
+	//DID WE RECEIVE COMMANDS?
+	if (Serial.available()) parseSerial();
 
 }
 
 
 void parseSerial() 
 {
-  char c = Serial.read();
+	char c = Serial.read();
 
-  switch (c) 
-  {
-  case 'p': 
-    printInfo();
-    break;
-  case 's': 
-    printSamples();
-    break;
-  case '+': 
-    THRESHOLD += 5;
-    break;             
-  case '-': 
-    THRESHOLD -= 5;
-    break;             
-  default:  
-    break;
-  }
+	switch (c) 
+	{
+	case 'p': 
+		printInfo();
+		break;
+	case 's': 
+		printSamples();
+		break;
+	case '+': 
+		THRESHOLD += 5;
+		break;						 
+	case '-': 
+		THRESHOLD -= 5;
+		break;						 
+	default:	
+		break;
+	}
 }
 
 
 void printSamples() 
 {
-  
-  Serial.print("BUFFSIZE: ");
-  Serial.print(BUFFERSIZE,DEC);
-  Serial.print(" Event: ");
-  Serial.println(event);
-  serialWrite(buffer,BUFFERSIZE);
-  Serial.flush();
-  
+	
+	Serial.print("BUFFSIZE: ");
+	Serial.print(BUFFERSIZE,DEC);
+	Serial.print(" Event: ");
+	Serial.println(event);
+	serialWrite(buffer,BUFFERSIZE);
+	Serial.flush();
+	
 }
 
 
@@ -135,32 +135,29 @@ void printSamples()
 //and I ended up loosing bytes. Maybe some form of flow-control should be used.
 void serialWrite(byte *buffer,int siz) 
 {
-  int kk;
-  for (kk=0;kk<siz;kk++) 
-  {
-    Serial.print(buffer[kk],HEX);    
-    Serial.print(" ");
-  }
-  Serial.println();
+	int kk;
+	for (kk=0;kk<siz;kk++) 
+	{
+		Serial.print(buffer[kk],HEX);		
+		Serial.print(" ");
+	}
+	Serial.println();
 }
 
 void printInfo() 
 {
-  totalTime = stopTime-startTime;
-  double samplesPerSec = i*1000.0/totalTime;
-  
-  Serial.print("T: ");
-  Serial.print(totalTime);
-  Serial.print(" Samples: ");
-  Serial.print(i,DEC);
-  Serial.print(" Samples/uSec: ");
-  Serial.print(samplesPerSec,7);
-  Serial.print(" Threshold: ");
-  Serial.println(THRESHOLD,DEC);
-  Serial.print("Loop time: ");
-  Serial.println(loopTime, DEC);
-  Serial.flush();
+	totalTime = stopTime-startTime;
+	double samplesPerSec = i*1000.0/totalTime;
+	
+	Serial.print("T: ");
+	Serial.print(totalTime);
+	Serial.print(" Samples: ");
+	Serial.print(i,DEC);
+	Serial.print(" Samples/uSec: ");
+	Serial.print(samplesPerSec,7);
+	Serial.print(" Threshold: ");
+	Serial.println(THRESHOLD,DEC);
+	Serial.print("Loop time: ");
+	Serial.println(loopTime, DEC);
+	Serial.flush();
 }
-
-
-

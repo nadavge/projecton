d50acd33ad87b9ec64de121a2aa32f2ca322938f
Nadav Geva <nadav1994@gmail.com>
2015-05-05 21:53:02 +0300

    Add call for function handling analysis
    Using master_of_puppets from Arzi's project

--- matlab/handle_event.m
+++ matlab/handle_event.m
@@ -2,7 +2,7 @@ function handle_event(raw_index)
 	NO_EVENT = -1
 	
 	index = hex32dec(raw_index);
-display('I am here!');
+
 	if index == NO_EVENT,
 		return
 	end
@@ -12,4 +12,6 @@ display('I am here!');
 	buffer3 = eval('buffer3');
 	buffer4 = eval('buffer4');
 	fs = eval('fs');
+	
+	master_of_puppets(buffer1, buffer2, buffer3, buffer4, fs);
 end

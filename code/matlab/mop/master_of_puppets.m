3867644959cbfcaed584ac0169dd9481d6231342
Nadav Geva <nadav1994@gmail.com>
2015-05-21 11:31:29 +0300

    Add documentation to MOP

--- code/matlab/mop/master_of_puppets.m
+++ code/matlab/mop/master_of_puppets.m
@@ -1,11 +1,18 @@
 function [direction] = master_of_puppets(sounds, fs)
+% MASTER_OF_PUPPETS Find the location of a gunfire based on sound
+%	sounds - a 4xBUFFERSIZE matrix of microphone recordings (each row is a mic)
+%	fs - the sample frequency of the microphones
+%	Using the sounds, use different sound analysis algorithms to receive
+%	the direction of the gunfire
+
     close all;
     
     arcLength = 4;
     tdoa = [0 0 0 0];
     
+	% Find the tdoa of each microphone relative to microphone no. 1
     for i = 2:4
-        [isShot, tdoa(i)] = find_delay(sounds(i,:),sounds(1,:), fs);
+        [isShot, tdoa(i)] = find_delay(sounds(i,:), sounds(1,:), fs);
 %         if (~isShot)
 %             direction = -1000;
 %             return

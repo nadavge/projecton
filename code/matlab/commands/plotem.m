f8f02d93eb73303253e11a1e524a1858c49e42a1
Roi Huminer <roi.huminer@mail.huji.ac.il>
2015-05-31 23:38:45 +0300

    Add control over plotem with vars

--- code/matlab/commands/plotem.m
+++ code/matlab/commands/plotem.m
@@ -1,12 +1,16 @@
-function plotem
+function plotem(which)
 % PLOTEM Plot the buffer by color
 %	Plots buffers by colors: red, green, blue, black
 	global buffer
-	colors = 'rgbk'
+	colors = 'rgbk';
 
+    if ~exist('which', 'var'),
+        which = 1:4;
+    end
+    
 	figure
 	hold on
-	for i = 1:4,
+	for i = which,
 		plot(buffer(i,:), colors(i))
 	end
 end

5abc70dbb92d7eec639b631ab9279a2b2d862c8c
Nadav Geva <nadav1994@gmail.com>
2015-06-03 17:56:25 +0300

    Add burned pin set to teensy
    The pins are read from the EEPROM, therfore
    saving the trouble of having to change the config
    on each code transfer

--- code/teensy/config.h
+++ code/teensy/config.h
@@ -1,7 +1,7 @@
 #ifndef _CONFIG_H
 #define _CONFIG_H
 
-#define VERSION "0.2.0"
+#define VERSION "0.3.0"
 /*
  * The frequency is calculated by dividing the number of samples by the time it took
  * to sample them. Since the time is in micros, the frequency is in MHz. Therefore,
@@ -24,28 +24,5 @@
 //TODO determine real value with yoel
 #define THRESHOLD_LIFE 1000
 #define THRESHOLD_MICS 2
-
-#define LO_SARUF 1 // [2,3,10,11] -> [1,2,3,4]
-#define SARUF 2 // [2,3,12,11] -> [1,2,3,4]
-
-#define TEENSY SARUF
-
-/* Pins USABLE on ADC0
- * 0, 1, ..., 13
- * Pins USABLE on ADC1
- * 2, 3, 10, 12, 13
- *
- * Channel 11 must be on ADC0, since on ADC1 it is disabled (See ADC_Module == 31)
- * This requires consideration when using the fast read, since the ADC0 should go first
- */
-
-#define PIN1 2
-#define PIN2 3
-#if TEENSY==LO_SARUF
-	#define PIN3 10
-#elif TEENSY==SARUF
-	#define PIN3 12
-#endif
-#define PIN4 11
 
 #endif //_CONFIG_H

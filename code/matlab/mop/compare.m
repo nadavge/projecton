d7a065274927e5ad50f5b3115e4568b6731b61f8
Shay Arzi <arzi.shay@gmail.com>
2015-06-03 23:09:50 +0300

    Update is shot checking
    Huminer

--- code/matlab/mop/compare.m
+++ code/matlab/mop/compare.m
@@ -1,5 +1,17 @@
-function result = compare(noise)
+function result = compare(buffer)
+threshold = 950;
 load('refNoise.mat')
-result = crosscor(ref,noise);
+
+mic_com = zeros(1,4);
+det_mic = 0;
+for k = 1:4
+    mic_com(k) = crosscor(ref,buffer(k,:));
+    if mic_com(k) > threshold
+        det_mic = det_mic + 1;
+    end
+end
+mic_mean = mean(mic_com) > threshold;
+mic_tot = det_mic >= 2;
+result = mic_tot && mic_mean;
 end
 
--- code/matlab/mop/crosscor.m
+++ code/matlab/mop/crosscor.m
@@ -1,16 +1,11 @@
 function result = crosscor(ref,sound)
-threshold = 1350;
-result = false;
-% TODO set real old_fs value from trial
-old_fs = 35000;
+
+old_fs = 37500;
 global fs;
 
 ref = freq_trans(ref, old_fs,fs);
 ref = ref/max(abs(ref));
 sound = sound/max(abs(sound));
 
-cross = max(real(xcorr(ref,sound)));
-if cross > threshold
-    result = true;
-end
+result = max(real(xcorr(ref,sound)));
 end

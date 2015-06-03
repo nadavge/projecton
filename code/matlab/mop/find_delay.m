3796d304805d9a8f0c6e606ce95a50742e86410a
Shay Arzi <arzi.shay@gmail.com>
2015-06-04 01:17:02 +0300

    Change compare logic
    Change in where we call compare

--- code/matlab/mop/find_delay.m
+++ code/matlab/mop/find_delay.m
@@ -1,9 +1,4 @@
-function [isShot, dt] = find_delay(data1, data2, fs)
-    isShot = compare(data1);
-%     if ~isShot
-%         dt = 0;
-%         return
-%     end
+function [ dt ] = find_delay(data1, data2, fs)
     len = length(data1) - 1;
     t = (-len:len)/fs;
     corr = xcorr(data1,data2);
